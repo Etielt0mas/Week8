8f55e88eff63fd22d96ef09a78a83906663a3970
Etielt0mas <117676023+Etielt0mas@users.noreply.github.com>
2022-11-08 07:51:20 +0000

    Update ExA.java

--- ExA.java
+++ ExA.java
@@ -2,27 +2,41 @@ import java.util.Scanner;
 
 public class ExA {
     static Scanner scanner = new Scanner(System.in);
-
-    public static void mediaNotas(int n){
+    public static double[] lerNotas(int n){
         double[] notas = new double[n];
-        double media = 0;
-        int failures = 0;
         for (int i = 0; i < n; i++) {
             notas[i] = scanner.nextDouble();
+        }
+        return notas;
+    }
+
+    public static double mediaCalculator(int n, double[] notas){
+        double media = 0;
+
+        for (int i = 0; i < n; i++) {
             media += notas[i];
+        }
+
+        return media/n;
+    }
+    public static int contarNeg(int n, double[] notas){
+
+        int failures = 0;
+        for (int i = 0; i < n; i++) {
             if (notas[i] < 10){
                 failures++;
             }
         }
-        media = media / n;
-        System.out.printf("average=%.1f%n", media);
-        System.out.println("failures="+failures);
+        return failures;
+
     }
 
     public static void main(String[] args) {
         // Pergunta o número de alunos
         int N = scanner.nextInt();
-        mediaNotas(N);
+        double[] notas = lerNotas(N);
+        System.out.printf("average=%.1f%n", mediaCalculator(N, notas));
+        System.out.println("failures="+contarNeg(N, notas));
     }
 
 }
